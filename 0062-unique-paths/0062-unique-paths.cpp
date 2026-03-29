37c4425a6fff79771c38a709be9dad68394f8dc6
Tanish326 <batratanish083@gmail.com>
2026-03-29 11:39:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-     
-     int check(int r,int c,int m,int n,vector<vector<int>>&dp){
-        if(r>m || c>n){
+    int check(int r, int c , int m , int n,vector<vector<int>>&dp){
+        if(c>=n || r>=m){
             return 0;
-        }
-        if(r==m-1 && c==n-1){
-            return 1;
         }
         if(dp[r][c]!=-1){
             return dp[r][c];
         }
-      int d =  check(r+1,c,m,n,dp);
-      int t =  check(r,c+1,m,n,dp);
-      return dp[r][c] = d+t;
-
-
-     }
+        if(r==m-1 && c==n-1){
+            return 1;
+        }
+        int down = check(r+1,c,m,n,dp);
+        int right = check(r,c+1,m,n,dp);
+        return down + right;
+    }
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+          return check(0,0,m,n,dp);
 
-       return check(0,0,m,n,dp);
+        
     }
 };
