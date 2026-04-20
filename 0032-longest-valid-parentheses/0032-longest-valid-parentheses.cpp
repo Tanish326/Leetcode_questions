33d22344b84abd611b08f41a3e0c4b1014f82c58
Tanish326 <batratanish083@gmail.com>
2026-04-20 18:01:38 +0530

    Time: 3 ms (40.12%), Space: 11.9 MB (11.78%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.size();
+        vector<int>dp(n,0);
+        int maxi = 0;
+        for(int i=1;i<n;i++){
+              if(s[i]==')'){
+                if(s[i-1]=='('){
+                    dp[i] = 2;
+                    if(i>=2){
+                        dp[i] += dp[i-2];
+                    }
+                }else{
+                    int prev = i-dp[i-1]-1;
+                    if(prev>=0 && s[prev]=='('){
+                        dp[i] = dp[i-1] + 2;
+                        if(prev-1>=0){
+                            dp[i] += dp[prev-1];
+                        }
+                    }
+                }
+              }
+           maxi = max(maxi,dp[i]);
+            
+        }
+     return maxi;
+    }
+};
