0b367451ee845d107b1e1054950f59b6a77cb7f5
Tanish326 <batratanish083@gmail.com>
2026-06-29 18:26:50 +0530

    Time: 82 ms (31.65%), Space: 146.4 MB (55.66%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -5,25 +5,18 @@ public:
         int pref = 0;
         unordered_map<int,int>mpp;
         mpp[0] = -1;
-        if(k==0){
-            for(int i=1;i<n;i++){
-                if(nums[i]==0 && nums[i-1]==0){
-                    return true;
-                }
-            }
-            return false;
-        }
-        
         for(int i=0;i<n;i++){
             pref = pref + nums[i];
-            int rem = pref%k;
-            if(mpp.find(rem)!=mpp.end()){
-                if(i-mpp[rem]>=2){
+            if(mpp.find(pref%k)!=mpp.end()){
+                if(i-mpp[pref%k]>=2){
                     return true;
                 }
-            }else{
-                mpp[rem] = i;
+
             }
+            if(mpp.find(pref%k)==mpp.end()){
+                mpp[pref%k] = i;
+            }
+
         }
         return false;
     }
