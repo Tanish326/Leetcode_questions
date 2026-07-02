c261ca6760c234be0e76e7d4f338aba1529ca422
Tanish Batra <batratanish083@gmail.com>
2026-07-02 17:36:18 +0530

    Time: 0 ms (100%), Space: 9.5 MB (5.84%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>>ans(n,vector<int>(n,0));
-         int left = 0;
-         int right = n-1;
-         int top = 0;
-         int bottom = n-1;
-         int val = 1;
-              while(left<=right && top<=bottom){
-         if(val<=n*n){
-          for(int i=left;i<=right;i++){
-                ans[top][i] = val;
+       vector<vector<int>>matrix(n,vector<int>(n,0));
+        int left = 0;
+        int right = n-1;
+        int top = 0;
+        int bottom = n-1;
+        int val = 1;
+        while(left<=right && top<=bottom){
+            for(int i=left;i<=right;i++){
+                matrix[top][i] = val;
                 val = val + 1;
-          }
-          top++;}
-          if(val<=n*n){
-          for(int i=top;i<=bottom;i++){
-            ans[i][right] = val;
-            val = val + 1;
-          }
-          right--;}
-          if(top<=bottom && val<=n*n){
-          for(int i=right;i>=left;i--){
-            ans[bottom][i] = val;
-            val = val + 1;
-          }
-          bottom--;}
-          if(left<=right && val<=n*n){
-          for(int i=bottom;i>=top;i--){
-            ans[i][left] = val;
-            val = val + 1;
-          }
-          left++;}}
-          return ans;
-         
+            }
+            top++;
+            for(int j=top;j<=bottom;j++){
+                matrix[j][right]= val;
+                val = val +1;
+            }
+            right--;
+            if(top<=bottom){
+            for(int i=right;i>=left;i--){
+                matrix[bottom][i] = val;
+                val = val + 1;
+            }
+            bottom--;}
+            if(left<=right){
+            for(int i=bottom;i>=top;i--){
+               matrix[i][left] = val;
+                val = val + 1;
+            }
+            left++;}
+        }
+        return matrix;
     }
 };
