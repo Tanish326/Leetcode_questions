0c4547b84ce69518e9e32f00c0cf44108f094a7f
Tanish Batra <batratanish083@gmail.com>
2026-07-05 18:04:17 +0530

    Time: 0 ms (100%), Space: 12.2 MB (97.19%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = 0;
-        int j = 0;
-        vector<int>ans;
-        while(i<m && j<n){
-            if(nums1[i]<=nums2[j]){
-                ans.push_back(nums1[i]);
+        int len = m + n;
+        int gap = (len / 2) + (len % 2);
+
+        while (gap > 0) {
+            int i = 0;
+            int j = i + gap;
+
+            while (j < len) {
+                // case 1: both pointers in nums1
+                if (i < m && j < m) {
+                    if (nums1[i] > nums1[j]) {
+                        swap(nums1[i], nums1[j]);
+                    }
+                }
+                // case 2: i in nums1, j in nums2
+                else if (i < m && j >= m) {
+                    if (nums1[i] > nums2[j - m]) {
+                        swap(nums1[i], nums2[j - m]);
+                    }
+                }
+                // case 3: both pointers in nums2
+                else {
+                    if (nums2[i - m] > nums2[j - m]) {
+                        swap(nums2[i - m], nums2[j - m]);
+                    }
+                }
+
                 i++;
-            }else if(nums2[j]<nums1[i]){
-                ans.push_back(nums2[j]);
                 j++;
             }
-         
+
+            if (gap == 1) break;
+            gap = (gap / 2) + (gap % 2);
         }
-        while(i<m){
-            ans.push_back(nums1[i]);
-            i++;
-        }
-        while(j<n){
-            ans.push_back(nums2[j]);
-            j++;
-        }
-        for(int i=0;i<ans.size();i++){
-            nums1[i] = ans[i];
+
+        // copy nums2 into nums1
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
         }
     }
 };
