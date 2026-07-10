ef18fe682bb0520a079e820abe43b6f599d69310
Tanish Batra <batratanish083@gmail.com>
2026-07-10 14:46:41 +0530

    Time: 74 ms (52.41%), Space: 105.4 MB (82.64%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+   double check(vector<int>&nums,int val){
+        int n = nums.size();
+        double count = 0;
+        for(int i=0;i<n-1;i++){
+            int t = ceil((double)nums[i]/val);
+            count = count + t;
+
+        }
+        count = count + (double)(nums[n-1])/val;
+        return count;
+    }
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        int n = dist.size();
+        int l = 1;
+        int h = 1e7;
+       
+        int ans = -1;
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            if(check(dist,mid)<=hour){
+                ans = mid;
+                h = mid-1;
+            }else{
+                l = mid+1;
+            }
+        }
+        return ans;
+    }
+};
