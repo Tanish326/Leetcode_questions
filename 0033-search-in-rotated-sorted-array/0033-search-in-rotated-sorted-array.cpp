b765c91670e6b66ebe755480e591e9ca802bc36d
Tanish Batra <batratanish083@gmail.com>
2026-07-11 09:57:52 +0530

    Time: 0 ms (100%), Space: 15.2 MB (67.6%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -5,17 +5,17 @@ public:
         int low = 0;
         int high = n-1;
         while(low<=high){
-            int mid = (low+high)/2;
+            int mid = low +(high-low)/2;
             if(nums[mid]==target){
                 return mid;
             }
-            else if(nums[mid]>=nums[low]){
+            if(nums[mid]>=nums[low]){
                 if(target>=nums[low] && target<=nums[mid]){
                     high = mid-1;
                 }else{
                     low = mid+1;
                 }
-            }else{
+            }else if(nums[mid]<=nums[high]){
                 if(target>=nums[mid] && target<=nums[high]){
                     low = mid+1;
                 }else{
