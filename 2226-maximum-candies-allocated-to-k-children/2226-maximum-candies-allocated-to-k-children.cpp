f4d0ac717b81c17fc8d34f1d8f455cc69c6a9817
Tanish Batra <batratanish083@gmail.com>
2026-07-13 00:35:33 +0530

    Time: 25 ms (61.26%), Space: 88.2 MB (70.4%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    long long check(vector<int>&candies,int val){
+    long long check(vector<int>&nums,int maxcandies){
+        int n = nums.size();
         long long count = 0;
-        for(int i=0;i<candies.size();i++){
-            count = count + candies[i]/val;
+        for(int i=0;i<n;i++){
+            count = count + nums[i]/maxcandies;
         }
         return count;
     }
     int maximumCandies(vector<int>& candies, long long k) {
-        int n = candies.size();
         int l = 1;
         int h = candies[0];
+        int n = candies.size();
+        int ans = 0;
         for(int i=0;i<n;i++){
             if(candies[i]>h){
                 h = candies[i];
             }
-           
         }
-        int ans = 0;
         while(l<=h){
             int mid = l + (h-l)/2;
-          if(check(candies,mid)>=k){
-            ans = mid;
-           l = mid+1;
-          }else{
-            h = mid-1;
-          }
+            if(check(candies,mid)>=k){
+                ans = mid;
+                l = mid+1;
+            }else{
+                h = mid-1;
+            }
         }
-         return ans;
+        return ans;
     }
 };
