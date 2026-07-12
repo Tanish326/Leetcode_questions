f215372d91231d08f995a887f8a538c00108bc90
Tanish Batra <batratanish083@gmail.com>
2026-07-13 00:02:05 +0530

    Time: 23 ms (5.31%), Space: 23 MB (14.64%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
-  long long check(int maxeat, vector<int>& piles, int val) {
-    long long count = 0;
-    for (int i = 0; i < piles.size(); i++) {
-        if (piles[i] <= val) {
-            count = count + 1;
-        } else {
-            if (piles[i] % val == 0) {
-                count = count + piles[i] / val;
-            } else {
-                count += 1 + piles[i] / val;
-            }
+    long long check(vector<int>&nums,int minval){
+        long long count = 0;
+        for(int i=0;i<nums.size();i++){
+            count = count + ceil((double)nums[i]/minval);
         }
+        return count;
     }
-    return count;
-}
-
     int minEatingSpeed(vector<int>& piles, int h) {
         int n = piles.size();
-        int l = 1;
-        int end = *max_element(piles.begin(), piles.end());
-        int ans = end;
-
-        while (l <= end) {
-            int mid = l + (end - l) / 2;
-
-            if (check(h,piles,mid) <= h) {
+        int i = 1;
+        int j = piles[0];
+        int ans;
+        for(int i=0;i<n;i++){
+            if(piles[i]>j){
+                j = piles[i];
+            }
+        }
+        while(i<=j){
+            int mid = i + (j-i)/2;
+            if(check(piles,mid)<=h){
                 ans = mid;
-                end = mid - 1;   // try smaller speed
-            } else {
-                l = mid + 1;     // need larger speed
+                j = mid-1;
+            }else{
+                i = mid+1;
             }
         }
         return ans;
