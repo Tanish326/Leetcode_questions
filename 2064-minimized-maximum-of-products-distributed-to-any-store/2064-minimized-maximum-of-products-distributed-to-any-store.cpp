e5a99c99425a10f37781e2439dafe073cf27e8b4
Tanish Batra <batratanish083@gmail.com>
2026-07-13 14:21:41 +0530

    Time: 95 ms (7.8%), Space: 88.5 MB (15.12%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int check(vector<int>&nums,int minvalue){
-        int count = 0;
-        for(int i=0;i<nums.size();i++){
-         count = count + ceil((double)nums[i]/minvalue);
-
-        }
-        return count;
+   int check(vector<int>&nums,int val){
+    int count = 0;
+    for(int i=0;i<nums.size();i++){
+        count = count + ceil((double)nums[i]/val);
     }
+    return count;
+   }
     int minimizedMaximum(int n, vector<int>& quantities) {
         int l = 1;
         int h = quantities[0];
-        for(int i=0;i<quantities.size();i++){
+        int n1 = quantities.size();
+        for(int i=0;i<n1;i++){
             if(quantities[i]>h){
                 h = quantities[i];
             }
         }
-    
         int ans;
         while(l<=h){
             int mid = l + (h-l)/2;
             if(check(quantities,mid)<=n){
                 ans = mid;
                 h = mid-1;
+
             }else{
                 l = mid+1;
             }
