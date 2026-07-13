015a33da328a0702b7518aca11ae3e0d30b5e164
Tanish Batra <batratanish083@gmail.com>
2026-07-13 16:10:26 +0530

    Time: 22 ms (58.5%), Space: 70.6 MB (12.36%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
-    long long check(vector<int>&nums,int mindays,int k){
-        int n = nums.size();
-        long long count = 0;
+    long long check(vector<int>&nums,int days,int k){
         int val = k;
-        for(int i=0;i<n;i++){
-           
-            if(mindays>=nums[i]){
-                val = val-1;
+        long long count = 0;
+        for(int i=0;i<nums.size();i++){
+            if(days>=nums[i]){
+                val--;
                 if(val==0){
-                count = count + 1;
-                val = k;
-            }
+                    count = count + 1;
+                    val = k;
+                }
             }else{
-                 val = k;
+                val = k;
             }
         }
         return count;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
         int n = bloomDay.size();
-        long long val = (long long)m*k;
-        if(val>n){
-            return -1;
-        }
         int l = 1;
         int h = bloomDay[0];
+        int ans;
+        long long v = (long long)m*k;
+        if(n<v){
+            return -1;
+        }
         for(int i=0;i<n;i++){
             if(bloomDay[i]>h){
                 h = bloomDay[i];
             }
         }
-        int ans = -1;
         while(l<=h){
-            int mid = l+(h-l)/2;
+            int mid = l + (h-l)/2;
             if(check(bloomDay,mid,k)>=m){
                 ans = mid;
                 h = mid-1;
