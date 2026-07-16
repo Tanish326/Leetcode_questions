fd8ae011dd4c2ffea6441bd26b6e8e1792f2d039
Tanish Batra <batratanish083@gmail.com>
2026-07-16 12:16:23 +0530

    Time: 97 ms (5.84%), Space: 155.6 MB (75.58%) - LeetHub

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
-    int gcd(int a, int b) {
-    while (b != 0) {
-        int r = a % b;
-        a = b;
-        b = r;
+  int find_gcd(int a, int b){
+    while(b){
+        int t = b;
+        b = a % b;
+        a = t;
     }
     return a;
 }
-   vector<int>finding_gcd(vector<int>&nums,vector<int>&max_values){
-       int n = nums.size();
-       vector<int>gcd_values(n);
-       for(int i=0;i<n;i++){
-           gcd_values[i] = gcd(nums[i],max_values[i]);
-       }
-       return gcd_values;
-   }
+ 
     long long gcdSum(vector<int>& nums) {
         int n = nums.size();
-        vector<int>max_values(n);
-        max_values[0] = nums[0];
-        for(int i=1;i<n;i++){
-            max_values[i] = max(max_values[i-1],nums[i]);
+        int maxi = nums[0];
+        vector<int>prefix(n);
+        for(int i=0;i<n;i++){
+            if(nums[i]>maxi){
+                maxi = nums[i];
+            }
+            prefix[i] = find_gcd(maxi,nums[i]);
         }
-        vector<int>ans;
-        long long int  sum = 0;
-     ans = finding_gcd(nums,max_values);
-        sort(ans.begin(),ans.end());
-        for(int i=0;i<n/2;i++){
-         sum +=  gcd(ans[i],ans[n-1-i]);
-        }
-        return sum;
-        
+        sort(prefix.begin(),prefix.end());
+      
+        long long sum = 0;
+         for(int i=0;i<n/2;i++){
+            sum = sum + find_gcd(prefix[i],prefix[n-1-i]);
+         }
+          return sum;
     }
 };
