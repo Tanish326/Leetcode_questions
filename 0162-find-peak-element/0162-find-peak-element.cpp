c87de5eaa9b19bf6cf007afb960400b3eb2d1eba
Tanish Batra <batratanish083@gmail.com>
2026-07-19 12:18:33 +0530

    Time: 0 ms (100%), Space: 12.5 MB (96.15%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -5,25 +5,25 @@ public:
         if(n==1){
             return 0;
         }
+        int l = 1;
+        int h = n-2;
         if(nums[0]>nums[1]){
             return 0;
         }
         if(nums[n-1]>nums[n-2]){
             return n-1;
         }
-        int l = 1;
-        int h = n-1;
         while(l<=h){
             int mid = l + (h-l)/2;
             if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
                 return mid;
             }
-            if(nums[mid]<nums[mid-1]){
-                h = mid-1;
+            if(nums[mid-1]>nums[mid]){
+                h = mid-1;  
             }else{
                 l = mid+1;
             }
         }
-        return -1;
+        return 0;
     }
 };
