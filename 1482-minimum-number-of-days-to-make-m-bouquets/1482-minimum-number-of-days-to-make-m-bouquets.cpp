f9ba7949c424bd0c00371e414fe82f18d43f0d30
Tanish Batra <batratanish083@gmail.com>
2026-07-19 17:39:44 +0530

    Time: 19 ms (81%), Space: 70.6 MB (12.35%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,44 +1,40 @@
 class Solution {
 public:
-    long long check(vector<int>&nums,int days,int k){
-        int val = k;
-        long long count = 0;
-        for(int i=0;i<nums.size();i++){
-            if(days>=nums[i]){
-                val--;
-                if(val==0){
-                    count = count + 1;
-                    val = k;
-                }
-            }else{
+   long long check(vector<int>&nums,int mindays,int k){
+     long long count = 0;
+    int n = nums.size();
+    int val = k;
+    for(int i=0;i<n;i++){
+        if(nums[i]<=mindays){
+            val--;
+            if(val==0){
+                count = count + 1;
                 val = k;
             }
+        }else{
+              val = k;
         }
-        return count;
     }
+    return count;
+   }
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int n = bloomDay.size();
-        int l = 1;
-        int h = bloomDay[0];
-        int ans;
-        long long v = (long long)m*k;
-        if(n<v){
+          int n = bloomDay.size();
+          long long val = (long long)m*k;
+          if(val>n){
             return -1;
-        }
-        for(int i=0;i<n;i++){
-            if(bloomDay[i]>h){
-                h = bloomDay[i];
-            }
-        }
-        while(l<=h){
-            int mid = l + (h-l)/2;
+          }
+          long long l = 1;
+          long long h = (long long)*max_element(bloomDay.begin(),bloomDay.end());
+          int ans = 0;
+          while(l<=h){
+            long long mid = l + (h-l)/2;
             if(check(bloomDay,mid,k)>=m){
                 ans = mid;
                 h = mid-1;
             }else{
-                l = mid+1;
+               l = mid+1;
             }
-        }
-        return ans;
+          }
+          return ans;
     }
 };
