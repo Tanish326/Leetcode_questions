fd92b154ef910bf5e7e8054601f84cd6db671974
Tanish Batra <batratanish083@gmail.com>
2026-07-19 11:57:39 +0530

    Time: 0 ms (100%), Space: 26.3 MB (35.31%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -11,19 +11,20 @@ public:
         if(nums[n-1]!=nums[n-2]){
             return nums[n-1];
         }
-        int i = 1;
-        int j = n-2;
-        while(i<=j){
-            int mid = i + (j-i)/2;
+        int l = 1;
+        int h = n-2;
+        while(l<=h){
+            int mid = l + (h-l)/2;
             if(nums[mid-1]!=nums[mid] && nums[mid]!=nums[mid+1]){
                 return nums[mid];
+
             }
-           if((mid%2==0 && nums[mid]==nums[mid+1])|| (mid%2==1 && nums[mid-1]==nums[mid])){
-                i = mid+1;
+            if(mid%2==0 && nums[mid]==nums[mid+1] || mid%2==1 && nums[mid]==nums[mid-1]){
+                l = mid+1;
             }else{
-                j = mid-1;
+                h = mid-1;
             }
         }
-        return -1;
+        return 0;
     }
 };
