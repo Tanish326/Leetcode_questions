d891c4205eaafdc9b3ff0fc69a52a3156f147a6d
Tanish Batra <batratanish083@gmail.com>
2026-07-20 14:50:19 +0530

    Time: 0 ms (100%), Space: 8.1 MB (72.88%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if((s.size()==goal.size()) && (s+s).find(goal)!=string::npos){
+            return true;
+        }
+        return false;
+        
+    }
+};
