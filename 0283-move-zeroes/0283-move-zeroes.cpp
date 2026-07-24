37def546290902c69aa43a44e08621f586ed5010
Tanish Batra <batratanish083@gmail.com>
2026-07-24 18:47:10 +0530

    Time: 0 ms (100%), Space: 23.8 MB (56.46%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -11,12 +11,10 @@ public:
         int j = 0;
         while(j<n){
             if(nums[j]!=0){
-               swap(&nums[i],&nums[j]);
+                swap(&nums[i],&nums[j]);
                 i++;
             }
             j++;
-
         }
-
     }
 };
