ae1e02c3bd5767db89106cea5f42085a287cf506
Tanish Batra <batratanish083@gmail.com>
2026-07-25 16:44:05 +0530

    Time: 0 ms (100%), Space: 23.6 MB (85.78%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,47 +1,48 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int el1 = -1e9;
-        int el2 = -1e9;
         int count1 = 0;
-        vector<int>ans;
         int count2 = 0;
-        for(int i=0;i<nums.size();i++){
-            if(count1==0 && el2!=nums[i]){
-                count1 = 1;
-                el1 = nums[i];
-            }
-           else if(count2==0 && el1!=nums[i]){
-                count2 = 1;
-                el2 = nums[i];
-            }else if(el1==nums[i]){
-                count1++;
-            }else if(el2==nums[i]){
+       
+        int n = nums.size();
+       
+        int val1 = -1e9;
+        int val2 = -1e9;
+        for(int i=0;i<n;i++){
+            if(nums[i]==val1){
+               count1++;
+            }else if( nums[i]==val2){
                 count2++;
-            }else{
+            }else if(count1==0 && nums[i]!=val2){
+                    val1 = nums[i];
+                    count1 = 1;
+                
+            }else if(count2==0 && nums[i]!=val1){
+                    val2 = nums[i];
+                    count2 = 1;
+                }
+            else{
                 count1--;
                 count2--;
+            }}
+        
+        count1=0;
+        count2=0;
+        for(int i=0;i<n;i++){
+            if(val1==nums[i]){
+                count1++;
+            }
+            if(val2==nums[i]){
+                count2++;
             }
         }
-    
-       count1 = 0;
-       count2 = 0;
-       for(int i=0;i<nums.size();i++){
-        if(el1==nums[i]){
-            count1++;
+        vector<int>ans;
+        if(count1>n/3){
+            ans.push_back(val1);
         }
-        if(el2==nums[i]){
-            count2++;
+        if(count2>n/3){
+            ans.push_back(val2);
         }
-       }
-       int n = nums.size();
-       int l = (int)n/3 + 1;
-       if(count1>=l){
-        ans.push_back(el1);
-       }
-       if(count2>=l){
-        ans.push_back(el2);
-       }
-       return ans;
+        return ans;
     }
 };
