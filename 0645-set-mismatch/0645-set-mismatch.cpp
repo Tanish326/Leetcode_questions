bbc624d4348c6fc00c95af3f36048245f3e695ba
Tanish Batra <batratanish083@gmail.com>
2026-07-25 08:39:40 +0530

    Time: 0 ms (100%), Space: 25 MB (98.34%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            int ind = abs(nums[i])-1;
+            if(nums[ind]<0){
+                ans.push_back(abs(nums[i]));
+            }else{
+                nums[ind] = -1*nums[ind];
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                ans.push_back(i+1);
+            }
+        }
+        return ans;
+    }
+};
