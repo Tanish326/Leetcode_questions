5177524ed442e7a3de1058146d5c65774700317a
Tanish Batra <batratanish083@gmail.com>
2026-07-25 13:44:41 +0530

    Time: 75 ms (78.97%), Space: 146.2 MB (85.73%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -6,18 +6,15 @@ public:
         unordered_map<int,int>mpp;
         mpp[0] = -1;
         for(int i=0;i<n;i++){
-            pref = pref + nums[i];
-            if(mpp.find(pref%k)!=mpp.end()){
+             pref = pref + nums[i];
+             if(mpp.find(pref%k)!=mpp.end()){
                 if(i-mpp[pref%k]>=2){
                     return true;
                 }
-
-            }
-            if(mpp.find(pref%k)==mpp.end()){
-                mpp[pref%k] = i;
-            }
-
+             }else{
+             mpp[pref%k] = i;}
         }
         return false;
     }
+
 };
