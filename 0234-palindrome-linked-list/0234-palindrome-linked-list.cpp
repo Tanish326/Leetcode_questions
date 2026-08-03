f02852e6bf55dac118c7293b076ddcf206d28f85
Tanish Batra <batratanish083@gmail.com>
2026-08-03 07:02:59 +0530

    Time: 0 ms (100%), Space: 117.9 MB (94.05%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,38 +10,45 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        if(!head){
-            return true;
-        }
+    ListNode* find(ListNode* head){
         ListNode* slow = head;
-        ListNode* fast = head;
-        
-        while(fast!=NULL && fast->next!=NULL){
+        ListNode* fast = head->next;
+        while(fast && fast->next){
             slow = slow->next;
             fast = fast->next->next;
         }
-        ListNode* temp = slow;
-        
+        return slow;
+    }
+    ListNode* reverse(ListNode* head){
         ListNode* prev = NULL;
-        while(temp!=NULL){
-            ListNode* a = temp->next;
-            temp->next = prev;
-            prev = temp;
-            temp = a;
+        while(head){
+            ListNode* a = head->next;
+            head->next = prev;
+            prev = head;
+            head = a;
         }
-        
-        
-        ListNode* a = head;
-        ListNode* b = prev;
-        while(b!=NULL){
-            if(a->val!=b->val){
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        if(!head){
+            return false;
+        }
+        if(!head->next){
+            return true;
+        }
+        ListNode* midprev = find(head);
+        ListNode* temp = midprev->next;
+        midprev->next = NULL;
+        ListNode* head1 = reverse(temp);
+        ListNode* temp1 = head;
+        while(temp1 && head1){
+            if(temp1->val!=head1->val){
                 return false;
             }
-            a = a->next;
-            b = b->next;
+            temp1 = temp1->next;
+            head1 = head1->next;
         }
+         return true;
 
-        return true;
     }
 };
