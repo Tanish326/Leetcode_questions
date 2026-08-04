dd9fb2ac4d2beddde1cacd04b5a2de7597de320a
Tanish Batra <batratanish083@gmail.com>
2026-08-04 21:18:59 +0530

    Time: 0 ms (100%), Space: 11.1 MB (58.8%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -0,0 +1,72 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+        ListNode* reverse(ListNode* head) {
+        ListNode* prev = NULL;
+        while (head) {
+            ListNode* f = head->next;
+            head->next = prev;
+            prev = head;
+            head = f;
+        }
+        return prev;
+    }
+
+    ListNode* findkthnode(ListNode* head, int k) {
+        while (head && --k) {
+            head = head->next;
+        }
+        return head;
+    }
+
+    ListNode* swapPairs(ListNode* head) {
+  
+        if (!head)
+            return head;
+
+        ListNode* temp = head;
+        ListNode* prevGroupTail = NULL;
+        ListNode* newHead = head;
+
+        while (temp) {
+            ListNode* kthnode = findkthnode(temp, 2);
+
+            // Less than k nodes remain
+            if (!kthnode) {
+                if (prevGroupTail)
+                    prevGroupTail->next = temp;
+                break;
+            }
+
+            ListNode* nextnode = kthnode->next;
+            kthnode->next = NULL;
+
+            // Reverse current group
+            ListNode* newGroupHead = reverse(temp);
+
+            // First group
+            if (temp == head) {
+                newHead = newGroupHead;
+            } else {
+                prevGroupTail->next = newGroupHead;
+            }
+
+            // Update pointers for next iteration
+            prevGroupTail = temp;
+            temp = nextnode;
+        }
+
+        return newHead;
+    }
+};
+
+   
