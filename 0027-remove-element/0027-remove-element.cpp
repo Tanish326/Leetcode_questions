7d3e0fc68f9e95f39559ad20fd26cd0ff53ffa50
Tanish Batra <batratanish083@gmail.com>
2026-08-21 13:58:14 +0530

    Time: 0 ms (100%), Space: 11.8 MB (49.21%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
-   void swap(int *a,int *b){
-    int t = *a;
-    *a = *b;
-    *b = t;
-   }
     int removeElement(vector<int>& nums, int val) {
           int n = nums.size();
           int i = 0;
           int j = 0;
           while(j<n){
             if(nums[j]!=val){
-                swap(&nums[i],&nums[j]);
+                nums[i] = nums[j];
                 i++;
             }
             j++;
