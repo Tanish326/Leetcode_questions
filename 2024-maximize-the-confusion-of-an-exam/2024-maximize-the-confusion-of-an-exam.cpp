6f7b61051f835c51819bbbda4dea81f7a19d9303
Tanish Batra <batratanish083@gmail.com>
2026-08-24 09:52:41 +0530

    Time: 9 ms (51.15%), Space: 12.6 MB (55.12%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int maxConsecutiveAnswers(string answerKey, int k) {
+           int n = answerKey.size();
+           int countT = 0;
+           int countF = 0;
+           int i = 0;
+           int j = 0;
+           int count = 0;
+           int maxlen = 0;
+           while(j<n){
+            if(answerKey[j]=='T'){
+                countT++;
+            }else{
+                countF++;
+            }
+            count = min(countT,countF);
+             if(count>k){
+            
+                while(i<n && count>k){
+                    if(answerKey[i]=='T'){
+                        countT--;
+                    }else{
+                        countF--;
+                    }
+                    count = count -  min(countT,countF);
+                    i++;
+
+                }
+             }
+             if(count<=k){
+                maxlen = max(maxlen,(j-i+1));
+             }
+             j++;
+
+           }
+           return maxlen;
+    }
+};
