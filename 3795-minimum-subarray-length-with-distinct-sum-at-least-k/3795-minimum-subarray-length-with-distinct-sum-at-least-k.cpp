285fa37e9bf200344b3a46912d2136adcbccbd4a
Tanish Batra <batratanish083@gmail.com>
2026-08-28 15:11:48 +0530

    Time: 419 ms (28.01%), Space: 317.9 MB (34.02%) - LeetHub

--- 3795-minimum-subarray-length-with-distinct-sum-at-least-k/3795-minimum-subarray-length-with-distinct-sum-at-least-k.cpp
+++ 3795-minimum-subarray-length-with-distinct-sum-at-least-k/3795-minimum-subarray-length-with-distinct-sum-at-least-k.cpp
@@ -1,42 +1,32 @@
 class Solution {
 public:
-   int minLength(vector<int>& nums, int k) {
-
-    int n = nums.size();
-
-    int i = 0;
-    long long sum = 0;
-    int minlen = INT_MAX;
-
-    unordered_map<int, int> mpp;
-
-    for (int j = 0; j < n; j++) {
-
-        // Add nums[j]
-        mpp[nums[j]]++;
-
-        // First occurrence -> add to distinct sum
-        if (mpp[nums[j]] == 1) {
-            sum += nums[j];
-        }
-
-        // Try to shrink
-        while (sum >= k) {
-
-            minlen = min(minlen, j - i + 1);
-
-            // Remove nums[i]
-            mpp[nums[i]]--;
-
-            // Completely removed -> subtract from distinct sum
-            if (mpp[nums[i]] == 0) {
-                sum -= nums[i];
+    int minLength(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int,int>mpp;
+        int sum = 0;
+        int i = 0;
+        int j = 0;
+        int minlen = 1e9;
+        while(j<n){
+            if(mpp.find(nums[j])==mpp.end()){
+                sum = sum + nums[j];
+               
             }
-
-            i++;
+            mpp[nums[j]]++;
+            while(i<=j && sum>=k){
+                minlen = min(minlen,(j-i+1));
+                mpp[nums[i]]--;
+                if(mpp[nums[i]]==0){
+                    sum = sum - nums[i];
+                    mpp.erase(nums[i]);
+                }
+                i++;
+            }
+            j++;
         }
+        if(minlen==1e9){
+            return -1;
+        }
+        return minlen;
     }
-
-    return minlen == INT_MAX ? -1 : minlen;
-}
 };
