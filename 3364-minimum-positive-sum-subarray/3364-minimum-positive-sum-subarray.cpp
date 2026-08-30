af116eb200509703ccf751ba5a6fc971cc720f96
Tanish Batra <batratanish083@gmail.com>
2026-08-30 15:21:56 +0530

    Time: 6 ms (12.98%), Space: 29.4 MB (69.11%) - LeetHub

--- 3364-minimum-positive-sum-subarray/3364-minimum-positive-sum-subarray.cpp
+++ 3364-minimum-positive-sum-subarray/3364-minimum-positive-sum-subarray.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int minimumSumSubarray(vector<int>& nums, int l, int r) {    int n = nums.size();
-            int minsum = 1e9;
+    int minimumSumSubarray(vector<int>& nums, int l, int r) {
+        int n = nums.size();
+        int minsum = 1e9;
         for(int i=0;i<n;i++){
-           int sum = 0;
-           for(int j=i;j<n;j++){
-            sum = sum + nums[j];
-            int len = j-i+1;
-            if(len>=l && len<=r){
-                 if(sum>0){
-                    minsum = min(minsum,sum);
-                 }
+            int sum = 0;
+            for(int j=i;j<n;j++){
+                sum = sum + nums[j];
+                if((j-i+1)>=l && (j-i+1)<=r){
+                    if(sum>0){
+                        minsum = min(minsum,sum);
+                    }
+                }
+
             }
-           }
         }
         if(minsum==1e9){
             return -1;
