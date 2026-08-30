fa3cb3de103339639051402ac685ac4154bcf5a0
Tanish Batra <batratanish083@gmail.com>
2026-08-30 11:37:11 +0530

    Time: 0 ms (100%), Space: 27.8 MB (33.28%) - LeetHub

--- 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.cpp
+++ 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int maximumStrongPairXor(vector<int>& nums) {
-          int n = nums.size();
-          int maxxor = 0;
-          for(int i=0;i<n;i++){
+        int n = nums.size();
+        int maxxor1 = 0;
+        for(int i=0;i<n;i++){
             int xor1 = 0;
             for(int j=i;j<n;j++){
                 if(abs(nums[i]-nums[j])<=min(nums[i],nums[j])){
-                         xor1 = nums[i]^nums[j];
-                         maxxor = max(maxxor,xor1);
-
+                    xor1 = nums[i]^nums[j];
+                    maxxor1 = max(maxxor1,xor1);
                 }
-              
             }
-          }
-          return maxxor;
+        }
+        return maxxor1;
     }
 };
