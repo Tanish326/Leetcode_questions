877eab2759ff1953dcae157ff9e90437f02dc12b
Tanish Batra <batratanish083@gmail.com>
2026-08-31 09:45:37 +0530

    Time: 4 ms (7.95%), Space: 22.6 MB (52.92%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
+    void swap(int *a,int *b){
+        int t = *a;
+        *a = *b;
+        *b = t;
+    }
     int removeDuplicates(vector<int>& nums) {
-        int n = nums.size();
-        int i = 0;
-        int j = 0;
-        while(j<n){
-            if(nums[i]!=nums[j]){
-                nums[i+1] = nums[j];
+         int n = nums.size();
+         int i = 0;
+         int j = 0;
+         while(j<n){
+            if(nums[j]!=nums[i]){
+                swap(&nums[i+1],&nums[j]);
                 i++;
             }
             j++;
-        }
-        return i+1;
+         }
+         return i+1;
+
     }
 };
