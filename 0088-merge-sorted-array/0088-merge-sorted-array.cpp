c534736ffaf950e0f63eb20b9bf99cefcbe8f53c
Tanish Batra <batratanish083@gmail.com>
2026-08-31 13:28:17 +0530

    Time: 0 ms (100%), Space: 12.4 MB (79.79%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,44 +1,30 @@
 class Solution {
 public:
+    void swap(int *a,int *b){
+        int t = *a;
+        *a = *b;
+        *b = t;
+    }
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int len = m + n;
-        int gap = (len / 2) + (len % 2);
-
-        while (gap > 0) {
-            int i = 0;
-            int j = i + gap;
-
-            while (j < len) {
-                // case 1: both pointers in nums1
-                if (i < m && j < m) {
-                    if (nums1[i] > nums1[j]) {
-                        swap(nums1[i], nums1[j]);
-                    }
-                }
-                // case 2: i in nums1, j in nums2
-                else if (i < m && j >= m) {
-                    if (nums1[i] > nums2[j - m]) {
-                        swap(nums1[i], nums2[j - m]);
-                    }
-                }
-                // case 3: both pointers in nums2
-                else {
-                    if (nums2[i - m] > nums2[j - m]) {
-                        swap(nums2[i - m], nums2[j - m]);
-                    }
-                }
-
-                i++;
-                j++;
+        int l = m-1;
+        int r = 0;
+       
+        while(l>=0 && r<n){
+            if(nums1[l]>nums2[r]){
+                swap(&nums1[l],&nums2[r]);
+               l--;
+            }else{
+                r++;
             }
-
-            if (gap == 1) break;
-            gap = (gap / 2) + (gap % 2);
+          
+          
         }
-
-        // copy nums2 into nums1
-        for (int i = 0; i < n; i++) {
-            nums1[m + i] = nums2[i];
+        sort(nums1.begin(),nums1.begin()+m);
+        sort(nums2.begin(),nums2.end());
+        
+        for(int i=0;i<n;i++){
+            nums1[i+m] = nums2[i];
         }
+        return;
     }
 };
