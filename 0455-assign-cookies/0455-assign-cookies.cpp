b80f23bc6402bb67ac178f9461b747154d654226
Tanish Batra <batratanish083@gmail.com>
2026-09-02 09:44:42 +0530

    Time: 3 ms (94.7%), Space: 44.9 MB (12.06%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -5,15 +5,14 @@ public:
         sort(s.begin(),s.end());
         int i = 0;
         int j = 0;
-        
-        while(i<g.size() && j<s.size()){
-            if(g[i]<=s[j]){
-        
-                i= i+1;
-
+        int n = g.size();
+        int m = s.size();
+        while(i<n && j<m){
+            if(s[j]>=g[i]){
+                i++;
             }
-            j = j+1;
-          
+            j++;
+
         }
         return i;
     }
