821def89cfb3b0e41cf8c9c9a74963b885b03958
Tanish Batra <batratanish083@gmail.com>
2026-09-03 13:53:50 +0530

    Time: 0 ms (100%), Space: 9.4 MB (26.83%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.cpp
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-  vector<int> shortestToChar(string s, char c) {
-    int n = s.size();
-    vector<int> ans(n, 1e9);
+    vector<int> shortestToChar(string s, char c) {
+        int n = s.size();
+        vector<int>ans(n,1e9);
+        int prev = -1;
+        for(int i=0;i<n;i++){
+            if(s[i]==c){
+                prev = i;
+            }
+            if(prev!=-1){
+            ans[i] = abs(i-prev);}
 
-    int dist = 1e9;
-
-    // Left → right
-    for (int i = 0; i < n; i++) {
-        if (s[i] == c)
-            dist = 0;
-        else if (dist != 1e9)
-            dist++;
-
-        ans[i] = dist;
+        }
+         prev = -1;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]==c){
+                prev = i;
+            }
+            if(prev!=-1){
+            ans[i] = min(ans[i],abs(i-prev));}
+        }
+        return ans;
     }
-
-    dist = 1e9;
-
-    // Right → left
-    for (int i = n - 1; i >= 0; i--) {
-        if (s[i] == c)
-            dist = 0;
-        else if (dist != 1e9)
-            dist++;
-
-        ans[i] = min(ans[i], dist);
-    }
-
-    return ans;
-}
 };
