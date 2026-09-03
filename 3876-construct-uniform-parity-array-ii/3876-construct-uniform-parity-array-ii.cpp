fc8c0852dc2734a7dccd2ab852bec152429ba91c
Tanish Batra <batratanish083@gmail.com>
2026-09-03 11:46:56 +0530

    Time: 59 ms (29.32%), Space: 165.9 MB (62.83%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
-   bool uniformArray(vector<int>& nums1) {
+    bool uniformArray(vector<int>& nums1) {
         sort(nums1.begin(),nums1.end());
         int n = nums1.size();
-        int count_even = 0;
-        int count_odd = 0;
-        for(int i=0;i<n;i++){
-            if(nums1[i]%2==0){
-                if(count_even > 0){
-                    count_even++;
-                }else if(count_odd > 0){
-                    count_odd++;
-                }else{
-                    count_even++;
-                }
-                
-            }else{
-                if(count_even > 0){
+        if((nums1[0]%2)==0){
+            for(int i=1;i<n;i++){
+                if(nums1[i]%2!=0){
                     return false;
-                }else if(count_odd > 0){
-                    count_odd++;
-                }else{
-                    count_odd++;
                 }
             }
             
+
         }
         return true;
+        
+
     }
 };
