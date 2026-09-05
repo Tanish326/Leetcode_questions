6f0e1e854277012a6fed0a6240943173ff0db8d0
Tanish Batra <batratanish083@gmail.com>
2026-09-05 13:58:37 +0530

    Time: 0 ms (100%), Space: 8.4 MB (66.72%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int  n = word.size();
+        int ind = -1;
+        for(int i=0;i<n;i++){
+            if(word[i]==ch){
+               ind = i;
+               break;
+            }
+        }
+        if(ind==-1){
+            return word;
+        }
+        reverse(word.begin(),word.begin()+ind+1);
+        return word;
+    }
+};
