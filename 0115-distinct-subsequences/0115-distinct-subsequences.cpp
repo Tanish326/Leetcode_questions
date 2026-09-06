e381e9bc5149b07f6d62accb6df22fd4ca55ca85
Tanish Batra <batratanish083@gmail.com>
2026-09-06 09:33:08 +0530

    Time: 31 ms (53.36%), Space: 26.9 MB (72.62%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
-    int numDistinct(string s, string t) {
-        int n = s.size();
-        int m = t.size();
-        vector<vector<long long>>dp(n+1,vector<long long>(m+1,0));
-         for(int i=0;i<=n;i++){
-            dp[i][0] = 1;
-         }
-         for(int i=1;i<=m;i++){
-            dp[0][i] = 0;
-         }
-         for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                 if(s[i-1]==t[j-1]){
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-                }else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-         }
-         return dp[n][m];
-        
+  int check(int i, int j, string &s, string &t,
+          vector<vector<int>>& dp) {
+
+    
+    if (j == t.size()) {
+        return 1;
     }
+
+    
+    if (i == s.size()) {
+        return 0;
+    }
+
+    if (dp[i][j] != -1) {
+        return dp[i][j];
+    }
+
+    int notake = check(i + 1, j, s, t, dp);
+
+    int take = 0;
+
+    if (s[i] == t[j]) {
+        take = check(i + 1, j + 1, s, t, dp);
+    }
+
+    return dp[i][j] = take + notake;
+}
+
+int numDistinct(string s, string t) {
+    int n = s.size();
+    int m = t.size();
+
+    vector<vector<int>> dp(n, vector<int>(m, -1));
+
+    return check(0, 0, s, t, dp);
+}
 };
