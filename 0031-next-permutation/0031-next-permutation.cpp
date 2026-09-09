ebe6c88c2f9ca798b46bd0d884aed22813821847
Tanish Batra <batratanish083@gmail.com>
2026-09-09 17:30:48 +0530

    Time: 0 ms (100%), Space: 15.7 MB (54.39%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
-    void swap(int *a, int *b){
-        int  t = *a;
-        *a = *b;
-        *b = t;
-    }
+    
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        //break point
         int ind = -1;
+        if(n==1){
+            return;
+        }
         for(int i=n-2;i>=0;i--){
-            if(nums[i+1]>nums[i]){
+            if(nums[i]<nums[i+1]){
                 ind = i;
                 break;
             }
+
         }
-        //no break point means last permutation so we reverse the array
         if(ind==-1){
             reverse(nums.begin(),nums.end());
             return;
         }
-        bool flag = false;
-       for(int i=ind;i>=0;i--){
-        for(int j=n-1;j>=ind;j--){
-            if(nums[j]>nums[i]){
-                flag = true;
-                swap(&nums[i],&nums[j]);
+        int l = ind;
+        int r = n-1;
+        while(l>=0 && r>ind){
+            if(nums[l]<nums[r]){
+              int t = nums[l];
+              nums[l] = nums[r];
+              nums[r] = t;
                 break;
+            }else{
+                r--;
             }
-            
+           
+
         }
-        if(flag){
-            break;
-        }
-       }
-            reverse(nums.begin()+ind+1,nums.end());
+        reverse(nums.begin()+ind+1,nums.end());
+        return;
     }
 };
