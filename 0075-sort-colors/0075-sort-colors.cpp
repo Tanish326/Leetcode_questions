fb97e7937b0a24ca1b0f3d33a6c9b4435286f972
Tanish Batra <batratanish083@gmail.com>
2026-09-13 11:56:41 +0530

    Time: 0 ms (100%), Space: 11.6 MB (79.55%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,26 +1,26 @@
 class Solution {
-public:    
-void swap(int *a,int *b){
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
-void sortColors(vector<int>& nums) {
+public:
+     void swap(int *a,int *b) {
+         int t = *a;
+         *a = *b;
+         *b = t;
+     }
+    void sortColors(vector<int>& nums) {
         int n = nums.size();
         int l = 0;
-        int mid = 0;
         int h = n-1;
-        while(l<=mid && h>=mid){
-            if(nums[mid]==0){
-                swap(&nums[l],&nums[mid]);
-                l++;
-                mid++;
-            }else if(nums[mid]==2){
-                swap(&nums[mid],&nums[h]);
-                h--;
-            }else{
-                mid++;
-            }
+        int m = 0;
+        while(m<=h){
+           if(nums[m]==2){
+            swap(&nums[m],&nums[h]);
+            h--;
+           }else if(nums[m]==0){
+            swap(&nums[l],&nums[m]);
+            l++;
+            m++;
+           }else if(nums[m]==1){
+            m++;
+           }
         }
 
     }
